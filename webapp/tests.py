f2cb98fcc5052ff2cba0a04d9a449a78bb810780
Jérôme Petazzoni <jerome.petazzoni@docker.com>
2015-03-02 14:27:41 -0800

    Update tests.py

--- webapp/tests.py
+++ webapp/tests.py
@@ -3,7 +3,7 @@ from app import app
 import os
 import unittest
 
-class AppTestCase(unittest.TestCase): mraaaa
+class AppTestCase(unittest.TestCase):
 
    def test_root_text(self):
         tester = app.test_client(self)
